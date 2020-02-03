2ccffdcee4c3ae92b1ca8e99ccd39481bae73fcd
oskina337 <aserr184@gmail.com>
2020-02-03 05:06:44 +0300

    Animation

---
+++ Assets/Script/CharacterAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            anim.SetBool("is run", true);      
+              }
+        else
+    {
+        anim.SetBool("is run", false);  
+    }
+    if (Input.GetKey(KeyCode.UpArrow)){
+        anim.SetTrigger("jump");
+    }
+}
+}
+  
